8276d509f344355d077b97f801599bc4f4a88bf1
david rose <aussiecoder0@gmail.com>
2021-10-20 15:46:57 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ input.onButtonPressed(Button.A, function () {
     } else {
         basic.showIcon(IconNames.No)
     }
+    wait = 0
 })
 input.onButtonPressed(Button.B, function () {
     if (number == 2) {
@@ -11,14 +12,19 @@ input.onButtonPressed(Button.B, function () {
     } else {
         basic.showIcon(IconNames.No)
     }
+    wait = 0
 })
+let wait = 0
 let number = 0
 number = 1
+wait = 1
 basic.showNumber(number)
 basic.showString("+")
 basic.showNumber(number)
 basic.showString("Press A")
+basic.pause(5000)
 number = 2
+wait = 1
 basic.showNumber(number)
 basic.showString("+")
 basic.showNumber(number)
